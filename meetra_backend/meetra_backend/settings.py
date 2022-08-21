f37cff25f8618c16c6d5961275dc252e229d30f3
Bello <angobello0@gmail.com>
2022-08-21 12:56:21 +0100

    increased access token lifetime from 5mins to an hour

--- meetra_backend/meetra_backend/settings.py
+++ meetra_backend/meetra_backend/settings.py
@@ -147,8 +147,8 @@ CORS_ALLOWED_ORIGINS = [
 
 # SimpleJWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60), # 1hour 
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
