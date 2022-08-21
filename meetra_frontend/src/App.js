02a0110cc327917b2bfa05d20b3a89e874dcf09d
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-08-21 03:29:58 +0100

    Create Feature Component

--- meetra_frontend/src/App.js
+++ meetra_frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import NavBar from './components/Nav/NavBar'
-
+import Feature from './components/features/Feature'
 export default function App() {
   return (
-    <>
-      <NavBar sticky="top" />       
+    <>          
       <Router>
+        <NavBar sticky="top" />
+        <Header />
+        <Feature /> 
         <Routes>
-          <Route exact path='/' element={<Header />} />
+          <Route exact path='/'  />
         </Routes>
       </Router>
     </>
---
+++ meetra_frontend/src/components/features/Feature.js
@@ -0,0 +1,54 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import IMG from '../../asset/user5.jpg'
+
+function Header() {
+  return (
+    <Container 
+      fluid 
+      id='feature' 
+      style={{ padding: '1.5rem 0 4rem 0' }}
+      className='border-bottom border-top' 
+    >
+      <Row 
+        className="align-items-center mx-auto" 
+        style={{marginTop: '2rem'}}
+      >
+        <Col mb-6 sm='11.5' md="6" lg="6">
+          <div className="section-container">
+            <div className="section-header">
+              <span className="section-title">Features</span>
+              <span className="section-subtitle">
+                Grow better, together
+              </span>
+              <h3>Grow together while enjoying our cool features.</h3>
+            </div>
+            <div className="section-body">
+              <span className="section-id">1</span>
+              <h2>Meeting Milestones - allocate times for section of your meetings.</h2>
+            </div>
+            <div className="section-body">
+              <span className="section-id">2</span>
+              <h2>Premium Security - enjoy security while in a meeting.</h2>
+            </div>
+            <div className="section-body">
+              <span className="section-id">3</span>
+              <h2>Meeting Reminder - be reminded of your meeting at least an hour before.</h2>
+            </div>
+          </div>
+        </Col>
+        
+        {/* Image Column */}
+        <Col sm='11.5' md="6" lg="6" style={{}}>
+          <div className="single-image-con ">
+            <img src={IMG} alt='Happy User' className='border-radius-l' />
+          </div>   
+        </Col>
+        
+      </Row>
+    </Container>
+  );
+}
+
+export default Header;  
